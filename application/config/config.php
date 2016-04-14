ca30b1cbfeecfe66fad9f6d971662413039ff818
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2016-04-14 01:09:37 +0100

    added the default url of the app

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://areasgrupo.alunos.di.fc.ul.pt/~psi018/';
 
 /*
 |--------------------------------------------------------------------------
