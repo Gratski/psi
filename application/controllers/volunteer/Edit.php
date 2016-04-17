adad4861ce5397c6d4d36772a1c2832f370878a6
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2016-04-17 17:30:09 +0100

    update the basic info

--- application/controllers/volunteer/Edit.php
+++ application/controllers/volunteer/Edit.php
@@ -1,20 +1,20 @@
 <?php
+
 require_once('VoluntarioController.php');
+
 /**
  * Created by PhpStorm.
  * User: JoaoR
  * Date: 16/04/16
  * Time: 14:52
  */
-class Edit extends VoluntarioController
-{
+class Edit extends VoluntarioController {
 
     /**
      * Edit constructor.
      * @see VoluntarioController
      */
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct();
     }
 
@@ -24,7 +24,7 @@ class Edit extends VoluntarioController
     /**
      * Obtem as areas de interesse de um dado voluntario
      */
-    public function areas(){
+    public function areas() {
         $this->load->model('volunteers/Areas_model', 'areas_model');
 
         $user = $this->session->user_details;
@@ -37,14 +37,13 @@ class Edit extends VoluntarioController
         $this->load->view('footer');
     }
 
-
     ///////////////////////////////////////////////////////////////////////////
     //                             PUT                                       //
     ///////////////////////////////////////////////////////////////////////////
     /**
      * Actualiza a agenda de um voluntário.
      */
-    public function put_schedule(){
+    public function put_schedule() {
 
         parse_str(file_get_contents('php://input'), $put);
 
@@ -59,14 +58,12 @@ class Edit extends VoluntarioController
         $this->load->model('schedule/Schedule_model', 'sm');
 
         //se actualizado
-        if($this->sm->update($horario_id, $horario))
-        {
+        if ($this->sm->update($horario_id, $horario)) {
             setFlash('success', 'Horario actualizado!');
             redirect('volunteer/myprofile');
         }
         //se nao actualizado
-        else
-        {
+        else {
             setFlash('danger', 'Ups.. algo correu mal. Tente novamente.');
             redirect('volunteer/edit/schedule');
         }
@@ -75,7 +72,7 @@ class Edit extends VoluntarioController
     /**
      * Adiciona uma area de interesse ao um voluntario
      */
-    public function put_areas(){
+    public function put_areas() {
 
         parse_str(file_get_contents('php://input'), $put);
         $volunteer = $put['utilizador'];
@@ -84,19 +81,19 @@ class Edit extends VoluntarioController
         $this->load->model('volunteers/Interests_model', 'im');
 
         //se inserida
-        if($this->im->add($volunteer, $area, $grupo)) {
+        if ($this->im->add($volunteer, $area, $grupo)) {
             setFlash('success', 'Interesse adicionado');
             redirect('volunteer/myprofile');
         }
         //se nao inserida
-        else{
+        else {
             setFlash('danger', 'Interesse nao adicionado');
             redirect('volunteer/edit/areas');
         }
     }
-    
-    public function basic (){
-         $this->load->model('volunteers/User_model', 'user_model');
+
+    public function basic() {
+        $this->load->model('volunteers/User_model', 'user_model');
 
         $user = $this->session->user_details;
         $user_info = $this->user_model->readUser($this->session->user_id);
@@ -105,6 +102,23 @@ class Edit extends VoluntarioController
         //gerar views
         $this->load->view('menu');
         $this->load->view('volunteer/edit/basic', $response);
+        $this->load->view('footer');
+    }
+
+    public function updateBasic() {
+        $this->load->model('volunteers/User_model', 'user_model');
+        $user = $this->session->user_details;
+        $info = array();
+
+        if (!(isset($_POST))) {
+            foreach ($_POST as $key => $value) {
+                $info = ['$key' => '$value'];
+            }
+        }
+        $response = "Yor information has been updated";
+        $this->user_model->updateUser($this->session->user_id, $info);
+        $this->load->view('menu');
+        $this->load->view('volunteer/myprofile', $response);
         $this->load->view('footer');
     }
 }
