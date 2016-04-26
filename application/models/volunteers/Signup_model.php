dccf87ff31f6553f6a72ecba3a25f776c510c659
João Luís Marques Rodrigues <JoaoR@Joaos-MacBook-Air.local>
2016-04-26 14:09:48 +0100

    added picture functional

--- application/models/volunteers/Signup_model.php
+++ application/models/volunteers/Signup_model.php
@@ -1,69 +1,4 @@
 <?php
-
-/**
- * Created by PhpStorm.
- * User: JoaoR
- * Date: 17/04/16
- * Time: 02:07
- */
-class Signup_model extends CI_Model
-{
-    /**
-     * Insere um novo utilizador em base de dados
-     * @param $data, dados de novo utilizador
-     */
-    public function insert($data){
-        $date = date('Y').'-'.date('m').'-'.date('d');
-        $newUser = array(
-            'nome' => $data['user']['nome'],
-            'email' => $data['user']['email'],
-            'password' => $data['user']['password'],
-            'telefone' => $data['user']['telefone'],
-            'criado_a' => $date,
-            'actualizado_a' => $date,
-            'freguesia' => $data['user']['freguesia']
-
-        );
-        $this->db->insert('Utilizador', $newUser);
-
-        //verifica se inseriu
-        $this->load->model('users/User_model', 'user_model');
-        $user = $this->user_model->getUserByEmail($newUser['email']);
-        if($user == null)
-            return false;
-        
-        //se eh voluntario
-        if($data['user_type'] == 'Volunteer')
-        {
-            $uploaded_foto = 0;
-            $foto = $data['foto'];
-            $folder = base_url()+'assets/img/users/'.$user->id;
-
-            //create directory
-            mkdir($folder, 0777);
-            if(!file_exists($folder))
-                return true;
-
-            $pictureDir = $folder . basename($foto['name']);
-            if(move_uploaded_file($foto['tmp_name'], $pictureDir))
-                $uploaded_foto = 1;
-
-            $this->db->insert('Voluntario', $data['volunteer']);
-        }
-        
-        return true;
-
-    }
-
-    private function uploadFile($file){
-        return true;
-    }
-
-}
-
-
-<?php
-
 /**
  * Created by PhpStorm.
  * User: JoaoR
@@ -111,15 +46,25 @@ class Signup_model extends CI_Model
 
             // se registou voluntário guarda a sua picture
             else{
-
+                echo "criou voluntário";
                 $uploaded_foto = 0;
-                $foto = $data['foto'];
-                $folder = base_url()+'assets/img/users/'.$user->id;
+                $foto = $data['user']['foto'];
 
+                $basePath = '../../'.base_url().'/assets';
+                if(file_exists($basePath))
+                    echo "ASSETS EXISTS";
+                else
+                    echo "ASSETS NOT EXISTS";
+
+                $folder = $basePath.'/img/users/'.$user->id.'/';
+
+                echo "BASE URL IS: " .$folder;
                 //create directory
-                mkdir($folder, 0777);
+                mkdir($folder, 0777, true);
                 if(!file_exists($folder))
-                    return true;
+                    return -1;
+
+                echo "CRIOU PASTA";
 
                 $pictureDir = $folder . basename($foto['name']);
                 if(move_uploaded_file($foto['tmp_name'], $pictureDir))
