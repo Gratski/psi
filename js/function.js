a8e8bf5d88db447da4447f0a05f5e188772c668a
JMBAlb <j.facetoface@gmail.com>
2016-04-26 14:25:13 +0100

    actualizado js

--- js/function.js
+++ js/function.js
@@ -58,16 +58,19 @@ function setFoto() {
 }
 
 var morada = {
-	Portugal: ["Lisboa", "Guarda"] ,
-	Lisboa: ["Lisboa","Sintra"],
+	Portugal: ["Lisboa", "Guarda"],
+	Lisboa: ["Alenquer","Amadora", "Arruda dos Vinhos", "Azambuja", "Cadaval", "Cascais", "Lisboa", "Loures", "Lourinhã", "Mafra", "Odivelas", "Oeiras", "Sintra", "Sobral de Monte Agraço", "Torres Vedras", "Vila Franca de Xira"],
 	Guarda: ["Aguiar da Beira","Almeida","Celorico da Beira", "Figueira de Castelo Rodrigo","Fornos de Algodres", "Gouveia","Guarda", "Manteigas", "Mêda","Pinhel","Sabugal","Seia","Trancoso", "Vila Nova de Foz Côa"],
 	Seia: ["Cabeça","Seia", "Santiago", "Sazes da Beira", "São Romão e Lapa dos Dinheiros", "Teixeira"]
 };
 
 function setDistritos() {
 	$('#distrito').empty();
+	$('#concelho').empty();
+	$('#freguesia').empty();
 	
 	var pais = document.getElementById("pais").value;
+	$("#distrito").append("<option>  </option>");
 	for(var i = 0; i < morada[pais].length; i++) {
 		$("#distrito").append("<option>" + morada[pais][i] + " </option>");
 	}
@@ -79,6 +82,7 @@ function setConcelhos() {
 	$('#concelho').empty();
 
 	var distrito = document.getElementById("distrito").value;
+	$("#concelho").append("<option>  </option>");
 	for(var i = 0; i < morada[distrito].length; i++) {
 		$("#concelho").append("<option>" + morada[distrito][i] + " </option>");
 	}
@@ -86,6 +90,35 @@ function setConcelhos() {
 
 function setFreguesias() {
 	$('#freguesia').empty();
+	$("#freguesia").append("<option>  </option>");
 	var concelho = document.getElementById("concelho").value;
 	$("#freguesia").append("<option>" + concelho + " </option>");
+}
+
+function mostraMorada() {
+	var show = document.getElementById("moradaForm").style.display;
+	if(show == "block") 
+		document.getElementById("moradaForm").style.display = "none";
+	else 
+		document.getElementById("moradaForm").style.display = "block";
+	
+}
+
+function showPass() {
+	var x = document.getElementById("passID").style.display;
+	if(x == "none") {
+		var x = document.getElementById("passID").style.display = "block";
+	}
+}
+
+function hidePass() {
+	var x = document.getElementById("passID").style.display;
+	if(x == "block") {
+		var x = document.getElementById("passID").style.display = "none";
+	}
+}
+
+function changeDate() {
+	var data = document.getElementById("dataN").value;
+	document.getElementById("changeDate").innerHTML= data;
 }
