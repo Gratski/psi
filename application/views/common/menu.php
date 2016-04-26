47c79a268a71354d7d3b20f9ca7278a3fec796c2
meemars <catbfca@gmail.com>
2016-04-26 13:31:02 +0100

    new link

--- application/views/common/menu.php
+++ application/views/common/menu.php
@@ -10,11 +10,11 @@
       </title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="../css/bs/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="../css/style.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url('/css/bs/css/bootstrap.min.css'); ?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url('/css/style.css');?>">
   <meta charset="UTF-8">
   <script src="http://code.jquery.com/jquery.min.js"></script>
-  <script src="../css/bs/js/bootstrap.min.js"></script>
+  <script src="<?php echo base_url('/css/bs/js/bootstrap.min.js'); ?>"></script>
   </head>
   <body>
     <nav class="navbar navbar-default">
@@ -23,14 +23,14 @@
     		<ul class="nav navbar-nav navbar-right">
     		  <li class="dropdown pull-right">
       			<a class="dropdown-toggle" data-toggle="dropdown" href="#" align="right">
-                      <img src="../img/profile.png" width="3%" height="3%">
+                      <img src="<?php echo base_url('/img/profile.png'); ?>" width="3%" height="3%">
                       <?php echo $username; ?>
       			<span class="caret"></span></a>
       			<ul class="dropdown-menu">
-                    <li><a href="Apps/PSI/views/volunteer/edit/basic.php">Editar Perfil</a></li>
-                    <li><a href="Apps/PSI/views/volunteer/edit/schedule.php">Editar Horário</a></li>
-                    <li><a href="Apps/PSI/views/volunteer/edit/areas.php">Editar Preferências</a></li>
-                    <li><a href="Apps/PSI/views/welcome/home.php">Logout (Sair)</a></li>
+                    <li><a href="<?php echo base_url('views/volunteer/edit/basic.php'); ?>">Editar Perfil</a></li>
+                    <li><a href="<?php echo base_url('views/volunteer/edit/schedule.php'); ?>">Editar Horário</a></li>
+                    <li><a href="<?php echo base_url('views/volunteer/edit/areas.php'); ?>">Editar Preferências</a></li>
+                    <li><a href="<?php echo base_url('views/welcome/home.php'); ?>">Logout (Sair)</a></li>
       			</ul>
     		  </li>
     		</ul>
