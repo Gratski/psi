7f2f7c3467e8e079e1f0b7f2d4569ddfa4d68f3e
João Luís Marques Rodrigues <JoaoR@Joaos-MacBook-Air.local>
2016-05-06 16:17:45 +0100

    adicionados campos a registo de instituição

---
+++ application/views/opportunities/single.php
@@ -0,0 +1,60 @@
+
+	
+		<div class="container">
+
+			<div class="row">
+				<div class="col-sm-8">
+					
+					<div class= "SectionTitle">
+						
+						<h1> Oportunidade de voluntariado </h1>
+
+						<!-- OFFER TITLE -->
+						<h2> <?php echo "OFFER TITLE HERE";?> </h2>
+						<span class="badge">area 1</span>
+						<span class="badge">area 2</span>
+						<span class="badge">area 3</span>
+						
+					</div>
+					
+					<!-- DESCRIPTION -->
+					<div class="well col-md-12">
+						<!-- POSITION -->
+						<p><?php echo "FUNCAO"; ?><p>
+
+						<!-- DECRIPTION ITSELF -->
+						<?php echo "OFFER DESCRIPTION GOES HERE"; ?>
+					</div>
+
+					<!-- ADDRESS COMPONENTS -->
+					<div class="col-md-12">
+						Nº de vagas: <?php echo "12"; ?><br>
+						Local: <?php echo "COUNTRY, DISTRITO, CIDADE, FREGUESIA"; ?><br>
+						Horário: <?php echo "DAS 8h00 às 12h00";?><br>
+						Periodo: <?php echo "A partir de dia 1 de Maio de 2016";?>
+					</div>
+					
+
+					<!-- PUBLISHED BY -->
+					<div class="col-md-12">
+						Publicado por:&nbsp;<?php echo "INSTITUTION NAME HERE"; ?><br>
+						Data:&nbsp;<?php echo "2 de Maio de 2016"; ?><br>
+					</div>
+
+					<!-- END -->
+					
+				</div>
+
+				<!-- OPORTUNIDADES QUE FAZEM MATCH EXCLUINDO ESTA -->
+				<div class="col-sm-4">
+					
+					<!-- LIST ITEM -->
+					<div class="col-md-12">
+						
+						<h5><?php echo "TITLE"; ?></h5>
+						<p><?php echo "area 1, area 2, area 3, ..."; ?></p>
+
+					</div>
+					
+				</div>
+							
