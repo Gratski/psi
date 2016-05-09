d728ab9b36181df702791ee3e07b83620a792bc3
João Luís Marques Rodrigues <JoaoR@Joaos-Air.lan>
2016-05-09 15:58:45 +0100

    adicionada a lista de oportunidades ao profile de instituicao

--- application/views/institution/myprofile.php
+++ application/views/institution/myprofile.php
@@ -1,13 +1,39 @@
 <div class="container">
-	
-	
+	<div class="row">
+		
 		<div class="col-md-2">
 		</div>
 		<div class="col-md-7">
-			<h4><!-- MATCHES DE VOLUNTARIOS --></h4>	
+			<h4>Lista de oportunidades</h4>
+
+			
+			<!-- Oportunidade  ciclo foreach aqui -->
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<a href="#"><b>Titulo de oportunidade com link para oportunidade</b></a>
+			  	</div>
+				<div class="panel-body">
+					<table class="table" style="font-size:13px;">
+						<tr>
+							<td><i class="glyphicon glyphicon-user"></i></td>
+							<td><a href="#">Nome de voluntário, idade</a></td>
+						</tr>
+						<tr>
+							<td><i class="glyphicon glyphicon-user"></i></td>
+							<td><a href="#">Nome de voluntário, idade</a></td>
+						</tr>
+						<tr>
+							<td><i class="glyphicon glyphicon-user"></i></td>
+							<td><a href="#">Nome de voluntário, idade</a></td>
+						</tr>
+					</table>
+			  	</div>
+			</div>
+
+
 		</div>
-		
-	
+
+	</div>
 
 </div>
 	
