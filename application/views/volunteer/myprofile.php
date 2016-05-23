b349f1042297562a88b621eadb4b756e5fc054e6
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2016-05-24 00:14:20 +0100

    fixing the href to get the opportunity

--- application/views/volunteer/myprofile.php
+++ application/views/volunteer/myprofile.php
@@ -1,42 +1,42 @@
 <div class="container">
-	<div class="row">
-		
-		<div class="col-md-2">
-		</div>
-		<div class="col-md-7">
+    <div class="row">
 
-			
-			<? if(hasFlash())
-				printFlash(); ?>
+        <div class="col-md-2">
+        </div>
+        <div class="col-md-7">
 
 
-			<!-- Oportunidades -->
-			<h4>Lista de oportunidades</h4>
-
-			
-			<!-- Oportunidade  ciclo foreach aqui -->
-			<div class="panel panel-default">
-				<div class="panel-body">
-					<table class="table" style="font-size:13px;">
-						<?php foreach( $opportunities as $op){ ?>
-						<tr>
-							<td>
-								<?php echo $op->instituicao; ?>
-							</td>
-							<td>
-								<a href="opportunity/view/single/"<?php echo $op->opID;?>""><?php echo $op->titulo; ?></a>
-							</td>
-							<td><?php echo $op->funcao; ?></td>
-							<td><?php echo $op->vagas; ?></td>
-						</tr>
-						<?php } ?>
-					</table>
-			  	</div>
-			</div>
+            <? if(hasFlash())
+            printFlash(); ?>
 
 
-		</div>
+            <!-- Oportunidades -->
+            <h2>Lista de oportunidades</h2>
 
-	</div>
+
+            <!-- Oportunidade  ciclo foreach aqui -->
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <table class="table" style="font-size:13px;">
+                        <?php foreach ($opportunities as $op) { ?>
+                            <tr>
+                                <td style="font-size: 28px;">
+                                    <?php echo $op->instituicao; ?>
+                                </td>
+                                <td style="font-size: 20px;">
+                                    <a href=" <?php echo base_url('index.php/opportunity/view/single/'); ?>/<?php echo $op->opID; ?>"><?php echo $op->titulo; ?></a>
+                                </td >
+                                <td style="font-size: 20px;"><?php echo $op->funcao; ?></td>
+                                <td style="font-size: 20px;"><?php echo $op->vagas; ?></td>
+                            </tr>
+                        <?php } ?>
+                    </table>
+                </div>
+            </div>
+
+
+        </div>
+
+    </div>
 
 </div>
