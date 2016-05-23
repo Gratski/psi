ad20b046a8830f1abca9939d1db0d86cb7f31482
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2016-05-24 00:16:15 +0100

    cleaning and format

--- application/views/institution/myprofile.php
+++ application/views/institution/myprofile.php
@@ -1,56 +1,50 @@
 <div class="container">
-	<div class="row">
-		
-		<div class="col-md-2">
-		</div>
-		<div class="col-md-7">
+    <div class="row">
 
-			
-			<? if(hasFlash())
-				printFlash(); ?>
-
-			<!-- descricao -->
-			<h4>Sobre nós</h4>
-			<span><?php echo $descricao; ?></span>
+        <div class="col-md-2">
+        </div>
+        <div class="col-md-7">
 
 
-			<!-- Oportunidades -->
-			<h2>Lista de oportunidades</h2>
+            <? if(hasFlash())
+            printFlash(); ?>
 
-			
-			<!-- Oportunidade  ciclo foreach aqui -->
-			<?php $i = -1; ?>
-			<?php foreach($oportunidades as $offer) { ?>
-				<div class="panel panel-default">
-					<div class="panel-heading">
-                                            <h2><a href=" <?php echo base_url('index.php/opportunity/view/single/'); ?>/<?php echo $offer->id;?>"><?php echo $offer->titulo; ?></a></h2>
-                                            <p style="font-size: 12px;"><?php echo $offer->descricao?></p>
-					</div>
-					<div class="panel-body">
-						<table class="table" style="font-size:13px;">
-							<?php $i += 1; ?>
-							<?php for($k = 0; $k < count($matchesPorOportunidade[$i]); $k++) { ?>
-							<tr>
-								<td><i class="glyphicon glyphicon-user"></i></td>
-								<td><a href="#"><?php echo $matchesPorOportunidade[$i][$k]['nome'] ?> </a></td>
-								<td><a href="#">Antonio </a></td>
-							<?php } ?>
-                                                                <td><i class="glyphicon glyphicon-user"></i></td>
-                                                                <td>Antonio</td>
-                                                                <td><i class="glyphicon glyphicon-user"></i></td>
-                                                                <td>Joao</td>
-							</tr>
-							
-						</table>
-					</div>
-				</div>
-				
-			<?php }?>
+            <!-- descricao -->
+            <h4>Sobre nós</h4>
+            <span><?php echo $descricao; ?></span>
 
 
-		</div>
+            <!-- Oportunidades -->
+            <h2>Lista de oportunidades</h2>
 
-	</div>
+
+            <!-- Oportunidade  ciclo foreach aqui -->
+            <?php $i = -1; ?>
+            <?php foreach ($oportunidades as $offer) { ?>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h2><a href=" <?php echo base_url('index.php/opportunity/view/single/'); ?>/<?php echo $offer->id; ?>"><?php echo $offer->titulo; ?></a></h2>
+                        <p style="font-size: 12px;"><?php echo $offer->descricao ?></p>
+                    </div>
+                    <div class="panel-body">
+                        <table class="table" style="font-size:13px;">
+                            <?php $i += 1; ?>
+                            <?php for ($k = 0; $k < count($matchesPorOportunidade[$i]); $k++) { ?>
+                                <tr>
+                                    <td><i class="glyphicon glyphicon-user"></i></td>
+                                    <td><a href="#"><?php echo $matchesPorOportunidade[$i][$k]['nome'] ?> </a></td>
+                                <?php } ?>
+                            </tr>
+
+                        </table>
+                    </div>
+                </div>
+
+            <?php } ?>
+
+
+        </div>
+
+    </div>
 
 </div>
-	
