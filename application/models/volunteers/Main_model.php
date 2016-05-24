5fe0550a269c7edd7ac1c0accb7e8d10f4a9e155
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2016-05-24 18:30:53 +0100

    fixed the view of the offer by the volunteer and the institution

--- application/models/volunteers/Main_model.php
+++ application/models/volunteers/Main_model.php
@@ -1,18 +1,11 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: JoaoR
- * Date: 17/04/16
- * Time: 02:07
- */
-class Main_model extends CI_Model
-{
+class Main_model extends CI_Model {
 
-	/**
-	*	obter informacao detalhada sobre um voluntario
-	*/
-	public function getVolunteerByEmail($email){
+    /**
+     * 	obter informacao detalhada sobre um voluntario a partir do email
+     */
+    public function getVolunteerByEmail($email) {
 
         $select = 'u.*, v.foto as foto, v.horario as horario, v.data_nascimento as data_nascimento, f.nome as freguesia, c.nome as concelho, d.nome as distrito, p.nome as pais';
         $query = $this->db->select($select)
@@ -26,12 +19,31 @@ class Main_model extends CI_Model
                 ->limit(1)
                 ->get();
         $res = $query->result();
-		
-        if (count($res) == 0){
-            return null;
-        }
-        else
-            return $res[0];
-	}
+
+
+        return (count($res) == 0) ? null : $res[0];
+    }
+
+    /**
+     * 	obter informacao detalhada sobre uma Instiruicao a partir do emal
+     */
+    public function getInstitutionByEmail($email) {
+
+        $select = 'u.*, f.nome as freguesia, c.nome as concelho, '
+                . 'd.nome as distrito, p.nome as pais';
+        $query = $this->db->select($select)
+                ->from('Utilizador u')
+                ->where('u.email', $email)
+                ->join('Instituicao i', 'i.utilizador=u.id')
+                ->join('Freguesia f', 'u.freguesia = f.id')
+                ->join('Concelho c', 'c.id = f.concelho')
+                ->join('Distrito d', 'd.id = c.distrito')
+                ->join('Pais p', 'p.id = d.pais')
+                ->limit(1)
+                ->get();
+        $res = $query->result();
+        
+        return (count($res) == 0) ? null : $res[0];
+    }
 
 }
