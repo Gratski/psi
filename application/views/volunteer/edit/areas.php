05927d545b7ba0644f6c0437d9020ca96b1ed3aa
JMBAlb <j.facetoface@gmail.com>
2016-05-24 18:36:42 +0100

    correçao das areas de interesse

--- application/views/volunteer/edit/areas.php
+++ application/views/volunteer/edit/areas.php
@@ -3,64 +3,64 @@
         
         <div class="row">
             <div class="col-sm-10">
-                <? if(hasFlash()){ ?>
+                <?php if(hasFlash()){ ?>
                     <div class="alert alert-success" role="alert">
-                        <? echo  printFlash(); ?>    
+                        <?php echo  printFlash(); ?>    
                     </div>
-                <? } ?>
+                <?php } ?>
             </div>
         </div>
 
         <div class="row">
             <div class="col-sm-10">
-                <? if(count($user_areas) != 0){ ?>
+                <?php if(count($user_areas) != 0){ ?>
 
                 <h3>Meus interesses</h3>
                 <table class="table">
                     
-                    <? foreach ($user_areas as $user_area) { ?>
+                    <?php foreach ($user_areas as $user_area) { ?>
                         <tr>
-                            <td><? echo $user_area->area_nome; ?></td>
-                            <td><? echo $user_area->grupo_tipo; ?></td>
+                            <td><?php echo $user_area->area_nome; ?></td>
+                            <td><?php echo $user_area->grupo_tipo; ?></td>
                             <td>
                                 <form action="deleteArea" method="post">
-                                    <input type="text" name="area_id" hidden="hidden" value="<? echo $user_area->area_id; ?>">
-                                    <input type="text" name="grupo_id" hidden="hidden" value="<? echo $user_area->grupo_id; ?>">
+                                    <input type="text" name="area_id" hidden="hidden" value="<?php echo $user_area->area_id; ?>">
+                                    <input type="text" name="grupo_id" hidden="hidden" value="<?php echo $user_area->grupo_id; ?>">
                                     <input type="submit" class="btn btn-sm btn-danger" value="remover">
                                 </form>
                             </td>
                         </tr>
-                    <? } ?>
+                    <?php } ?>
 
                 </table>
-                <? } ?>
+                <?php } ?>
             </div>
         </div>
     
 
         <div class="row">
             <div class="col-sm-10">
-                <? if(count($user_areas_complement) != 0){ ?>
+                <?php if(count($user_areas_complement) != 0){ ?>
 
                 <h3>Adicionar interesses</h3>
                 <table class="table">
                     
-                    <? foreach ($user_areas_complement as $user_area) { ?>
+                    <?php foreach ($user_areas_complement as $user_area) { ?>
                         <tr>
-                            <td><? echo $user_area->area_nome; ?></td>
-                            <td><? echo $user_area->grupo_tipo; ?></td>
+                            <td><?php echo $user_area->area_nome; ?></td>
+                            <td><?php echo $user_area->grupo_tipo; ?></td>
                             <td>
                                 <form action="addArea" method="post">
-                                    <input type="text" name="area_id" hidden="hidden" value="<? echo $user_area->area_id; ?>">
-                                    <input type="text" name="grupo_id" hidden="hidden" value="<? echo $user_area->grupo_id; ?>">
+                                    <input type="text" name="area_id" hidden="hidden" value="<?php echo $user_area->area_id; ?>">
+                                    <input type="text" name="grupo_id" hidden="hidden" value="<?php echo $user_area->grupo_id; ?>">
                                     <input type="submit" class="btn btn-sm btn-success" value="adicionar">
                                 </form>
                             </td>
                         </tr>
-                    <? } ?>
+                    <?php } ?>
 
                 </table>
-                <? } ?>
+                <?php } ?>
             </div>
         </div>
         
